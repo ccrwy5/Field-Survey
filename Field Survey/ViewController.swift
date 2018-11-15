6c830428d1f71658ec75013b6a73f13d6bbc883c
Chris Rehagen <ccrwy5@mail.missouri.edu>
2018-11-14 21:34:30 -0600

    First commit

---
+++ Field Survey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Field Survey
+//
+//  Created by Chris Rehagen on 11/14/18.
+//  Copyright © 2018 Chris Rehagen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
